1dec2326c5d1e9077662824e5f05470cc210516b
SonjaReimannThanisch <137377644+Sonja40@users.noreply.github.com>
2024-11-18 14:16:13 +0100

    createed the 2 overlay button, addeed some templates , small JS and css

--- scripts/overlay.js
+++ scripts/overlay.js
@@ -7,4 +7,32 @@ document.getElementById('btn-about').addEventListener('click', function () {
     document.getElementById('about-page').classList.remove('d-none');
     document.getElementById('contact-overlay').classList.add('d-none');
 });
-s
+
+
+function showNotificationAndButtons() {
+    let container = document.getElementById('notification-container');
+    let notificationHtml = createNotificationHtml("Contact successfully created", "success");
+    let actionButtonsHtml = createActionButtonsHtml();
+    container.innerHTML = notificationHtml + actionButtonsHtml;
+}
+  
+
+function showNotificationAndButtons() {
+    let container = document.getElementById('dynamic-container');
+    let notificationHtml = `s
+        <div class="notification">
+            Contact successfully created
+        </div>`;
+    let buttonsHtml = `
+        <div class="action-buttons">
+            <button class="btn-action">
+                <img src="./icons/edit.svg" alt="Edit" class="icon"> Edit
+            </button>
+            <button class="btn-action">
+                <img src="./icons/delete.svg" alt="Delete" class="icon"> Delete
+            </button>
+        </div>`;
+    container.innerHTML = notificationHtml + buttonsHtml;
+}
+
+document.getElementById('submit-button').addEventListener('click', showNotificationAndButtons);
