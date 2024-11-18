506cdafa624b008ed59d28a5fc23dcb6a29ba7e7
charlybraun1809 <charly.braun1809@gmail.com>
2024-11-18 21:33:59 +0100

    updated inputFields

--- scripts/addTask.js
+++ scripts/addTask.js
@@ -1,22 +1,24 @@
 document.addEventListener("DOMContentLoaded", function () {
-    let select = document.getElementById('dropdown-arrow');
+    let select = document.getElementById('assignedToDropdown');
     let placeHolder = document.getElementsByClassName('dropdown-selected')[0];
     let isClicked = false;
     let arrow = document.querySelector('#dropdown-arrow');
     let dropDown = document.getElementsByClassName('dropdown-list')[0];
-    select.addEventListener('click', () => {
-        if (!isClicked) {
-            arrow.style.transform = "translateY(-50%) rotate(180deg)";
-            placeHolder.querySelector('span').textContent = 'An'
-            dropDown.style.display = 'block';
-            isClicked = true;
-        } else {
-            arrow.style.transform = "translateY(-50%) rotate(0deg)";
-            placeHolder.querySelector('span').textContent = 'select contact';
-            dropDown.style.display = 'none';
-            isClicked = false;
+    select.addEventListener('click', (event) => {
+        if (event.target === arrow || event.target === placeHolder) {
+            if (!isClicked) {
+                arrow.style.transform = "translateY(-50%) rotate(180deg)";
+                placeHolder.querySelector('span').textContent = 'An'
+                dropDown.style.display = 'block';
+                isClicked = true;
+            } else {
+                arrow.style.transform = "translateY(-50%) rotate(0deg)";
+                placeHolder.querySelector('span').textContent = 'select contact';
+                dropDown.style.display = 'none';
+                isClicked = false;
+            }
         }
-    })
+        })
 });
 
 
