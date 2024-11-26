9efbb63dd91dca52f9151e195a6a8c209a611dc3
SonjaReimannThanisch <137377644+Sonja40@users.noreply.github.com>
2024-11-26 19:05:00 +0100

    first try, database

--- script.js
+++ script.js
@@ -10,3 +10,21 @@ async function loadData() {
     let responseToJson = response.json();
     console.log(response);
 }
+
+async function uploadContacts(contactsData) {
+    try {
+        let response = await fetch(BASE_URL + "contacts.json", {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(contactsData),
+        });
+        if (!response.ok) throw new Error("Fehler beim Hochladen der Kontakte");
+        console.log("Kontakte erfolgreich hochgeladen:", await response.json());
+    } catch (error) {
+        console.error("Error beim Hochladen der Kontakte:", error);
+    }
+}
+
+uploadContacts(contacts);
