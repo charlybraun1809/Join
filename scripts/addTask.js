5f999dd8befd467fbc3e35322a84f58320d5679f
SonjaReimannThanisch <137377644+Sonja40@users.noreply.github.com>
2025-01-14 01:45:37 +0100

    fix

--- scripts/addTask.js
+++ scripts/addTask.js
@@ -18,6 +18,14 @@ async function init() {
     changeSubtaskImg();
     sendSubtaskForm();
     enableGlobalSubmit();
+    const dateInput = document.querySelector("#date");
+    if (!dateInput.hasAttribute("data-flatpickr-initialized")) {
+        flatpickr("#date", {
+            dateFormat: "dd/mm/yy",
+            allowInput: true
+        });
+        dateInput.setAttribute("data-flatpickr-initialized", "true");
+    }
 };
 
 document.addEventListener('DOMContentLoaded', init);
